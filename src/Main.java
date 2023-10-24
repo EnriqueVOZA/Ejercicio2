d668a5942b8f6acb23d0f72319dd48c4a03b9cc0
EnriqueVOZA <enriquevoza@gmail.com>
2023-10-24 16:28:55 +0200

    ejercicio2segundocommit

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         boolean married = false;
         double height = 1.87;
 
-
+        //Si ponemos valores incompatibles con los tipos declarados no compila y da errores.
         System.out.println("El nombre es: " + name + ", la edad es: " + age + ", su estado civil es casado? " + married + ", y su altura es " + height);
     }
 }
