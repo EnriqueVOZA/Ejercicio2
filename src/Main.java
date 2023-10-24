534df78f41c56b43f16ae6f0dded81af013264c9
EnriqueVOZA <enriquevoza@gmail.com>
2023-10-24 16:25:47 +0200

    ejercicio2

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String name = "Enrique";
+        int age = 34;
+        boolean married = false;
+        double height = 1.87;
+
+
+        System.out.println("El nombre es: " + name + ", la edad es: " + age + ", su estado civil es casado? " + married + ", y su altura es " + height);
+    }
+}
